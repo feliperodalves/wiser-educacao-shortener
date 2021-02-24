3aed0f9d69c901dbf7ffa0b529803c7cc484cfdb
Felipe Alves <felipe.rod.alves@gmail.com>
2021-02-24 02:58:40 -0300

    fix: deploy e build with babel

---
+++ babel.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@modules': './src/modules',
+          '@infra': './src/infra',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+    'babel-plugin-transform-typescript-metadata',
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+  ],
+};
